da34c7ed8caa49357d72ec6844f9a7df2ce1f61a
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-04-09 13:49:30 +0600

    solve

--- binary.ts
+++ binary.ts
@@ -1,18 +1,16 @@
 function binarySearch(arr: number[], target: number): number {
-    let low = 0;
-    let high = arr.length - 1;
-
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-
-        if (arr[mid] === target) {
-            return mid; // target found
-        } else if (arr[mid] < target) {
-            low = mid + 1; // search right half
-        } else {
-            high = mid - 1; // search left half
-        }
+  let array = arr.sort((a, b) => a - b);
+  let high = array.length - 1;
+  let low = 0;
+  while (low <= high) {
+    let mid = Math.floor((high + low) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (target > array[mid]) {
+      low = mid + 1;
+    } else if (target < array[mid]) {
+      high = mid - 1;
     }
-
-    return -1; // target not found
+  }
+  return -1;
 }
