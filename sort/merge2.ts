55a56d274b5877d67395ff2ca43218587f04878d
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-04-16 11:38:51 +0600

    merge

---
+++ sort/merge2.ts
@@ -0,0 +1,25 @@
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(left: number[], right: number[]): number[] {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+let arr2 = [6, 1, 5, 9, 0, 2, 7];
+console.log(mergeSort(arr2));
