bdc7d01577a987b9c2af6aa7ebb17c7342d83413
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-05-06 09:49:44 +0600

    maxlen

--- twoPointer/LongestOnes.ts
+++ twoPointer/LongestOnes.ts
@@ -1,13 +1,13 @@
 function longestOnes(arr: [], k: number): number {
-  let left = 0;
   let zeros = 0;
+  let left = 0;
   let maxLen = 0;
   for (let right = 0; right < arr.length; right++) {
     if (arr[right] === 0) zeros++;
 
     while (zeros > k) {
-      if (nums[left] === 0) zeros--;
-      left++;
+      if (arr[left] === 0) left--;
+      zeros++;
     }
     maxLen = Math.max(maxLen, right - left + 1);
   }
